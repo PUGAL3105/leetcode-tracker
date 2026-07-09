4dac286c5c9ba1a89366c9aad019b60347196a6b
PUGAL3105 <pugalarasan0310@gmail.com>
2026-07-09 09:48:41 +0530

    Create 682-BaseballGame

---
+++ 682-BaseballGame/682-BaseballGame.java
@@ -0,0 +1,39 @@
+// Last updated: 09/07/2026, 09:48:41
+class Solution {
+    public int calPoints(String[] operations) {
+               Stack<Integer> stack = new Stack<>();
+
+        for (String op : operations) {
+
+            if (op.equals("+")) {
+                int a = stack.pop();
+                int b = stack.peek();
+
+                stack.push(a);
+                stack.push(a + b);
+
+            } else if (op.equals("D")) {
+
+                stack.push(2 * stack.peek());
+
+
+            } else if (op.equals("C")) {
+
+                stack.pop();
+
+            } else {
+
+                stack.push(Integer.parseInt(op));
+            }
+        }
+
+        int sum = 0;
+
+        for (int score : stack) {
+            sum += score;
+        }
+
+        return sum;
+ 
+    }
+}
