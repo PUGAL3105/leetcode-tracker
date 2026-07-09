3b3ed6f9460dfa4ca46da57def4056009c90c8c1
PUGAL3105 <pugalarasan0310@gmail.com>
2026-07-09 09:47:58 +0530

    Create 1127-LastStoneWeight

---
+++ 1127-LastStoneWeight/1127-LastStoneWeight.java
@@ -0,0 +1,25 @@
+// Last updated: 09/07/2026, 09:47:58
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+
+        int n = stones.length;
+
+        if (n == 1) {
+            return stones[0];
+        }
+
+        while (true) {
+
+            Arrays.sort(stones);
+
+            int y = stones[n - 1];
+            int x = stones[n - 2];
+
+            if (x == 0) {
+                return y;
+            }
+            stones[n - 1] = y - x;
+            stones[n - 2] = 0;
+        }
+    }
+}
