0ab632cd90218b628eff00f1bd7d8951ff675326
PUGAL3105 <pugalarasan0310@gmail.com>
2026-07-09 09:51:21 +0530

    Create 142-LinkedListCycleIi

---
+++ 142-LinkedListCycleIi/142-LinkedListCycleIi.java
@@ -0,0 +1,23 @@
+// Last updated: 09/07/2026, 09:51:21
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) break;
+        }
+
+        if (fast == null || fast.next == null) return null;
+
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return slow;        
+    }
+}
