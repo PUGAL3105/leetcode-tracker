5648c62a22fd4288d017a2b3394b2653c6733c83
PUGAL3105 <pugalarasan0310@gmail.com>
2026-07-09 09:51:05 +0530

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.java
@@ -0,0 +1,9 @@
+// Last updated: 09/07/2026, 09:51:05
+class Solution {
+    public int majorityElement(int[] nums) {
+    
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+       
+    }
+}
