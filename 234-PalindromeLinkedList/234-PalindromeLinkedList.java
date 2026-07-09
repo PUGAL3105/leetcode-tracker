8ae51dc222d333c70fe67e402973cceb7238c3b1
PUGAL3105 <pugalarasan0310@gmail.com>
2026-07-09 09:50:33 +0530

    Create 234-PalindromeLinkedList

---
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.java
@@ -0,0 +1,24 @@
+// Last updated: 09/07/2026, 09:50:33
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> arr = new ArrayList<>();
+
+        while (head != null) {
+            arr.add(head.val);
+            head = head.next;
+        }
+
+        int left = 0;
+        int right = arr.size() - 1;
+
+        while (left < right) {
+            if (!arr.get(left).equals(arr.get(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;        
+    }
+}
