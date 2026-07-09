e507654026fc82e4c10d3dcd1da1a8f3872272b4
PUGAL3105 <pugalarasan0310@gmail.com>
2026-07-09 09:47:34 +0530

    Create 1961-MaximumIceCreamBars

---
+++ 1961-MaximumIceCreamBars/1961-MaximumIceCreamBars.java
@@ -0,0 +1,17 @@
+// Last updated: 09/07/2026, 09:47:34
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count = 0;
+        for (int i = 0; i < costs.length; i++) {
+            if (coins >= costs[i]) {
+                coins = coins - costs[i];
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        return count;
+    }
+}
