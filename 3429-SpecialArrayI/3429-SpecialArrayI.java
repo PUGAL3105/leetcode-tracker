0217c39c309decc1e40a2fed7310a036ae86fef4
PUGAL3105 <pugalarasan0310@gmail.com>
2026-07-09 09:47:04 +0530

    Create 3429-SpecialArrayI

---
+++ 3429-SpecialArrayI/3429-SpecialArrayI.java
@@ -0,0 +1,11 @@
+// Last updated: 09/07/2026, 09:47:03
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]% 2 == nums[i+1]%2){
+                return false;
+            }
+        }
+        return true;
+    }
+}
