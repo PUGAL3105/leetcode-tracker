fda727bff95849655a3f86f1a67bb7f6a254b2d8
PUGAL3105 <pugalarasan0310@gmail.com>
2026-07-09 09:48:53 +0530

    Create 561-ArrayPartition

---
+++ 561-ArrayPartition/561-ArrayPartition.java
@@ -0,0 +1,11 @@
+// Last updated: 09/07/2026, 09:48:52
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int s=0;
+        for(int i=0;i<nums.length;i+=2){
+            s +=nums[i];
+        }
+        return s;
+    }
+}
