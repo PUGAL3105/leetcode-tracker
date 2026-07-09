a71d37f565af0c701d6948d4fcea962af94aac8b
PUGAL3105 <pugalarasan0310@gmail.com>
2026-07-09 09:49:04 +0530

    Create 477-TotalHammingDistance

---
+++ 477-TotalHammingDistance/477-TotalHammingDistance.java
@@ -0,0 +1,19 @@
+// Last updated: 09/07/2026, 09:49:04
+class Solution {
+    public int totalHammingDistance(int[] nums) {
+        int n = nums.length;
+        int total = 0;
+        for (int bit = 0; bit < 32; bit++) {
+            int ones = 0;
+            for (int num : nums) {
+                if ((num & (1 << bit)) != 0) {
+                    ones++;
+                }
+            }
+            int zeros = n - ones;
+            total += ones * zeros; 
+        }
+
+        return total;
+    }
+}
