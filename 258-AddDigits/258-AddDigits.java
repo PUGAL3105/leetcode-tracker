1460c267474002d33cf491ce967849fb77d77104
PUGAL3105 <pugalarasan0310@gmail.com>
2026-07-09 09:50:28 +0530

    Create 258-AddDigits

---
+++ 258-AddDigits/258-AddDigits.java
@@ -0,0 +1,10 @@
+// Last updated: 09/07/2026, 09:50:28
+class Solution {
+    public int addDigits(int num) {
+      if(num==0){
+      return 0;
+      }
+      return 1+(num-1)%9;
+        
+    }
+}
