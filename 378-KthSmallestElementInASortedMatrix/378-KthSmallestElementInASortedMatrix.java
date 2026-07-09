1ed2734747846b2bb4d14aa2379a0bc50248be13
PUGAL3105 <pugalarasan0310@gmail.com>
2026-07-09 09:49:58 +0530

    Create 378-KthSmallestElementInASortedMatrix

---
+++ 378-KthSmallestElementInASortedMatrix/378-KthSmallestElementInASortedMatrix.java
@@ -0,0 +1,19 @@
+// Last updated: 09/07/2026, 09:49:58
+
+
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        int [] arr = new int[n*n];
+        int idx = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                arr[idx++] = matrix[i][j];
+            }
+        }
+        
+        Arrays.sort(arr);
+        
+        return arr[k - 1];
+    }
+}
