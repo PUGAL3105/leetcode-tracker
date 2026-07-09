99834a2f554acd3905f665b9673d99e702da6f63
PUGAL3105 <pugalarasan0310@gmail.com>
2026-07-09 09:52:52 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -0,0 +1,12 @@
+// Last updated: 09/07/2026, 09:52:52
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i] >= target)
+            return i;
+        }
+        return nums.length;
+        
+    }
+}
