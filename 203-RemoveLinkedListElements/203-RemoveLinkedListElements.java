75370eefc0f6492a9f16bd6b4663aae0aa3042a7
PUGAL3105 <pugalarasan0310@gmail.com>
2026-07-09 09:50:51 +0530

    Create 203-RemoveLinkedListElements

---
+++ 203-RemoveLinkedListElements/203-RemoveLinkedListElements.java
@@ -0,0 +1,16 @@
+// Last updated: 09/07/2026, 09:50:51
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode ans = new ListNode(0, head);
+        ListNode dummy = ans;
+
+        while (dummy != null) {
+            while (dummy.next != null && dummy.next.val == val) {
+                dummy.next = dummy.next.next;
+            }
+            dummy = dummy.next;
+        }
+
+        return ans.next;        
+    }
+}
