178eaf4e6df4a77c30bda8274f2d69c4bb8182da
PUGAL3105 <pugalarasan0310@gmail.com>
2026-07-09 09:49:45 +0530

    Create 396-RotateFunction

---
+++ 396-RotateFunction/396-RotateFunction.java
@@ -0,0 +1,21 @@
+// Last updated: 09/07/2026, 09:49:45
+class Solution {
+    public int maxRotateFunction(int[] A) {
+        int sum = 0, F = 0;
+        int n = A.length;
+
+        for (int i = 0; i < n; i++) {
+            sum += A[i];
+            F += i * A[i];
+        }
+
+        int max = F;
+
+        for (int i = 1; i < n; i++) {
+            F += sum - n * A[n - i];
+            max = Math.max(max, F);
+        }
+
+        return max;
+    }
+}
