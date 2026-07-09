ccc7c79c1fd1ed4462c166adf56468689a63faf2
PUGAL3105 <pugalarasan0310@gmail.com>
2026-07-09 09:46:51 +0530

    Create 3931-ProcessStringWithSpecialOperationsI

---
+++ 3931-ProcessStringWithSpecialOperationsI/3931-ProcessStringWithSpecialOperationsI.java
@@ -0,0 +1,22 @@
+// Last updated: 09/07/2026, 09:46:51
+class Solution {
+    public String processStr(String s) {
+        StringBuilder ans = new StringBuilder();
+        for(char ch : s.toCharArray()) {
+            if(ch == '*' && ans.length() >= 1) {
+                ans.deleteCharAt(ans.length() - 1);
+            }
+            else if(ch == '#' && ans.length() >= 1) {
+                String temp = ans.toString();
+                ans.append(temp);
+            }
+            else if(ch == '%') {
+                ans.reverse();
+            }
+            if(ch >= 'a' && ch <= 'z') {
+                ans.append(ch);
+            }
+        }
+        return ans.toString();
+    }
+}
